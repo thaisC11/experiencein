a22dbd43efb0df7dd7623852781256eeb69854e2
Thaís Carvalho <thais.cb11@gmail.com>
2021-11-21 18:02:03 -0300

    módulo 3 - a página perfil

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 def index(request):
     return render(request, 'index.html')
 
-def exibir(request):
-    return render(request, 'perfil.html')
+def exibir(request, perfil_id):
+    perfil = Perfil()
 
-# Create your views here.
+    if perfil_id == 1:
+        perfil = Perfil('Thaís Carvalho', 'thais@email.com', '12345532', 'Eldorado')
+    if perfil_id == 2:
+        perfil = Perfil('Gabriel Fernandes', 'gabriel@email.com', '23554321', 'CFA')
+    return render(request, 'perfil.html', {'perfil': perfil})
