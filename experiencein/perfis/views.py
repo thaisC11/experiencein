ade9b328dcbb52eafee26bab999583ed980ce26c
Thaís Carvalho <thais.cb11@gmail.com>
2021-11-21 18:28:07 -0300

    módulo 4 - primeiros passos com persistência

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -5,10 +5,6 @@ def index(request):
     return render(request, 'index.html')
 
 def exibir(request, perfil_id):
-    perfil = Perfil()
 
-    if perfil_id == 1:
-        perfil = Perfil('Thaís Carvalho', 'thais@email.com', '12345532', 'Eldorado')
-    if perfil_id == 2:
-        perfil = Perfil('Gabriel Fernandes', 'gabriel@email.com', '23554321', 'CFA')
-    return render(request, 'perfil.html', {'perfil': perfil})
+    perfil = Perfil.objects.get(id=perfil_id)
+    return render(request, 'perfil.html', { "perfil" : perfil})
